4aebfe238ac40f2ce097d344a9fdc66ad152044c
Marko Pellikka <eemapel@gmail.com>
2020-03-22 16:01:50 -0700

    Clean-up and use black and white stones

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function IconUmbrella() {
-   return (
-     <svg className="umbrella" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-labelledby="title">
-       <path d="M27 14h5c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0zM27 14c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0 14c0 1.112-0.895 2-2 2-1.112 0-2-0.896-2-2.001v-1.494c0-0.291 0.224-0.505 0.5-0.505 0.268 0 0.5 0.226 0.5 0.505v1.505c0 0.547 0.444 0.991 1 0.991 0.552 0 1-0.451 1-0.991v-14.009c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-5.415 6.671-9.825 15-9.995v-1.506c0-0.283 0.224-0.499 0.5-0.499 0.268 0 0.5 0.224 0.5 0.499v1.506c8.329 0.17 15 4.58 15 9.995h-5z"/>
-      </svg>
-   )
-}
-
-function IconHeart() {
-  return (
-    <svg viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
-      <path fill="none" stroke="red"
-        d="M 10,30
-          A 20,20 0,0,1 50,30
-          A 20,20 0,0,1 90,30
-          Q 90,60 50,90
-          Q 10,60 10,30 z" />
-    </svg>
-  )
-}
-
 function Inter() {
   return (
     <svg viewBox="0 0 25 25">
@@ -45,21 +24,10 @@ function InterStone(props) {
         M 0,12
         L 24,12
       " />
-      <circle cx="12" cy="12" r="10" stroke="black" strokeWidth="2" fill={props.stoneColor} />
+      <circle cx="12" cy="12" r="11" stroke="black" strokeWidth="1" fill={props.stoneColor} />
     </svg>
   )
 }
-
-/*class Square extends React.Component {
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}*/
 
 function Square(props) {
   return (
@@ -80,7 +48,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = <InterStone stoneColor={ (this.state.xIsNext ? "yellow" : "red") } />;
+    squares[i] = <InterStone stoneColor={ (this.state.xIsNext ? "black" : "white") } />;
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
