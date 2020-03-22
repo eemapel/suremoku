8f7c473717d9fcd8db580b1607e50e69c49d08b3
Marko Pellikka <eemapel@gmail.com>
2020-03-22 13:41:38 -0700

    Working with state from Board

--- src/index.js
+++ src/index.js
@@ -2,30 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+/*class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
+}*/
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        squares: Array(15 * 15).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
 
   renderRow(row, size) {
     var rows = [];
 
     for (var x = 0; x < size; x++) {
-      rows.push(<Square value={row * size + x} />)
+      let idx = row * size + x;
+      rows.push(<Square value={this.state.squares[idx]} onClick={ () => this.handleClick(idx) } />)
     }
 
     return rows
