5a71671c9e49b4d302207a02bc5ecf5f342fa454
Marko Pellikka <eemapel@gmail.com>
2020-05-17 13:18:04 -0700

    Express webserver added

--- client/src/index.js
+++ client/src/index.js
@@ -51,11 +51,23 @@ class BoardUI extends React.Component {
     };
   }
 
+  getResponse = async() => {
+      const response = await fetch('api/hello');
+      const body = await response.json();
+      if (response.status !== 200) throw Error(body.message);
+
+      return body;
+  }
+
   handleClick(i) {
     const squares = this.state.squares.slice();
 
     // Do nothing if square not empty
     if (!G.isEmpty(i)) return
+
+    // Test express server
+    this.getResponse()
+      .then(res => { console.log(res) })
 
     let cond = G.setMove(i)
 
